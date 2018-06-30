193dd13f023df941260f911cb565ebbffcdb0e10
Arth <arth.dharaskar97@gmail.com>
2018-06-30 12:05:07 +0530

    Update hand_detect.py
    Updated hand_detect.py to draw countours around hand

--- hand_detect.py
+++ hand_detect.py
@@ -11,37 +11,28 @@ import cv2
 min_YCrCb = np.array([0, 133, 77], np.uint8)
 max_YCrCb = np.array([255, 173, 127], np.uint8)
 
-cv2.namedWindow('Camera Output')
 
-cap = cv2.VideoCapture(0)
+frame = cv2.imread('')
+temp = frame.copy()
+# Convert image to YCrCb
+imageYCrCb = cv2.cvtColor(frame, cv2.COLOR_BGR2YCR_CB)
 
-while True:
-    ret, frame = cap.read()
+# Find region with skin tone in YCrCb image
+skinRegion = cv2.inRange(imageYCrCb, min_YCrCb, max_YCrCb)
 
-    temp = frame.copy()
-    grayscaled = cv2.cvtColor(temp, cv2.COLOR_BGR2GRAY)
-    # Convert image to YCrCb
-    imageYCrCb = cv2.cvtColor(frame, cv2.COLOR_BGR2YCR_CB)
+# Do contour detection on skin region
+image, contours, _ = cv2.findContours(skinRegion, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Find region with skin tone in YCrCb image
-    skinRegion = cv2.inRange(imageYCrCb, min_YCrCb, max_YCrCb)
+# Draw the contour on the source image
+for i, c in enumerate(contours):
+    area = cv2.contourArea(c)
+    if area > 1000:
+        cv2.drawContours(temp, contours, i, (0, 0, 0))
 
-    # Do contour detection on skin region
-    image, contours, _ = cv2.findContours(skinRegion, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# Display the source image
+cv2.imshow('test', temp)
 
-    # Draw the contour on the source image
-    for i, c in enumerate(contours):
-        area = cv2.contourArea(c)
-        if area > 1000:
-            cv2.drawContours(grayscaled, contours, i, (0, 0, 0), thickness=cv2.FILLED)
-
-    ret, threshold = cv2.threshold(grayscaled, 10, 255, cv2.THRESH_BINARY)
-    # Display the source image
-    fin = cv2.cvtColor(threshold, cv2.COLOR_GRAY2BGR)
-    cv2.imshow('Camera Output', fin)
-
-    # Check for user input to close program
-    keyPressed = cv2.waitKey(1)  # wait 1 milisecond in each iteration of while loop
-
+# Check for user input to close program
+keyPressed = cv2.waitKey(1)  # wait 1 milisecond in each iteration of while loop
+cv2.waitKey(0)
 cv2.destroyAllWindows()
-cv2.release()
