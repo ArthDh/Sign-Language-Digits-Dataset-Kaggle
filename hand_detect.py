b4c3a6c922805b524a707d2e094e1492666f9db5
Arth <arth.dharaskar97@gmail.com>
2018-07-03 12:15:31 +0530

    Update hand_detect.py

--- hand_detect.py
+++ hand_detect.py
@@ -1,5 +1,7 @@
 import numpy as np
+import os
 import cv2
+import shutil
 
 # TODO:
 # Pass dataset through mask
@@ -14,19 +16,33 @@ class ImageGen:
     max_YCrCb = np.array([255, 173, 127], np.uint8)
     foreground_imgs = []
     background_imgs = []
+    final_ds_path = "/Users/arth/Desktop/test_ds"
 
     def __init__(self, foreground_imgs, background_imgs, n_imgs):
-        self.foreground_imgs = foreground_imgs
-        self.background_imgs = background_imgs
-        self.n_imgs = n_imgs
-        self.countoured_images = []
+        self.foreground_imgs = foreground_imgs  # Pass handsign(i)
+        self.background_imgs = background_imgs  # Pass dtd
+        self.n_imgs = n_imgs  # Number of images to generate
+        self.countoured_images = []  # Storing generated images
 
-    def draw_contours(self):
-        for img in self.foreground_imgs:
-            ogi_img = cv2.imread(img)
+    def get_random_sign(self):
+        index = np.random.randint(len(self.foreground_imgs) - 1)
+        return self.foreground_imgs[index]
 
+    def get_random_bg(self):
+        index = np.random.randint(len(self.background_imgs) - 1)
+        return self.background_imgs[index]
+
+    def get_images(self):
+        if not os.path.exists(self.final_ds_path):
+            os.mkdir(self.final_ds_path)
+
+        for i in range(self.n_imgs):
+
+            img_path = self.get_random_sign()
+            bg_img_path = self.get_random_bg()
+
+            ogi_img = cv2.imread(img_path)
             rows, cols, channels = ogi_img.shape
-            print(rows, cols, channels)
             # Copies of original image
             contoured_image = ogi_img.copy()
             copy_image = ogi_img.copy()
@@ -42,7 +58,7 @@ class ImageGen:
             img_no_bg = cv2.bitwise_and(ogi_img, ogi_img, mask=masked_img)
 
             # Background image
-            bg_img = cv2.imread(self.background_imgs[0])
+            bg_img = cv2.imread(bg_img_path)
             bg_img = bg_img[0:rows, 0:cols]
 
             bg_mask = cv2.bitwise_not(masked_img)
@@ -51,21 +67,7 @@ class ImageGen:
 
             temp = img_no_bg + bg_img
 
-            # Display image with no bg
-            cv2.imshow('Countoured: ', temp)
-            # Check for user input to close program
-            keyPressed = cv2.waitKey(1)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
-
-    def mask_background(self):
-        pass
-
-    def add_background(self):
-        pass
-
-    def generate_images(self):
-        pass
+            cv2.imwrite("{}/{}.jpg".format(self.final_ds_path, i), temp)
 
 
 if __name__ == '__main__':
@@ -73,10 +75,18 @@ if __name__ == '__main__':
     hand_images = []
     background_imgs = []
 
-    h1_path = "/Users/arth/Desktop/ML/projects/Sign Language/Dataset/test/0/IMG_4069.JPG"
-    b1_path = "/Users/arth/Desktop/backgroud_ds/banded_0008.JPG"
-    hand_images.append(h1_path)
-    background_imgs.append(b1_path)
+    h1_path = "/Users/arth/Desktop/ML/projects/Sign Language/Dataset/test/1/"
+    b1_path = "/Users/arth/Desktop/backgroud_ds/"
 
-    imgen = ImageGen(hand_images, background_imgs, 1)
-    imgen.draw_contours()
+    for imgs in os.listdir(h1_path):
+        if not imgs.startswith('.'):
+            path = os.path.join(h1_path, imgs)
+            hand_images.append(path)
+
+    for imgs in os.listdir(b1_path):
+        if not imgs.startswith('.'):
+            path = os.path.join(b1_path, imgs)
+            background_imgs.append(path)
+
+    imgen = ImageGen(hand_images, background_imgs, 10)
+    imgen.get_images()
